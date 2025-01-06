25b1552b73059078c8a07e605c3addd7325382f7
Steven Trotter <stevetrot@gmail.com>
2025-01-06 10:30:40 +0000

    Added test and handling of new data

--- policy-manager/policy-manager.go
+++ policy-manager/policy-manager.go
@@ -2,6 +2,7 @@ package policy_manager
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/hashicorp/go-hclog"
 	"github.com/open-policy-agent/opa/rego"
@@ -26,6 +27,7 @@ func New(ctx context.Context, logger hclog.Logger, bundlePath string) *PolicyMan
 func (pm *PolicyManager) Execute(ctx context.Context, pluginNamespace string, input map[string]interface{}) ([]Result, error) {
 	var output []Result
 
+	pm.logger.Debug("Executing policy", "input", input)
 	regoArgs := []func(r *rego.Rego){
 		rego.Query("data.compliance_framework"),
 		rego.Package(fmt.Sprintf("compliance_framework.%s", pluginNamespace)),
@@ -44,34 +46,6 @@ func (pm *PolicyManager) Execute(ctx context.Context, pluginNamespace string, in
 			continue
 		}
 
-		controls := []Control{}
-
-		for _, annotation := range module.Annotations {
-			if annotation.Custom["controls"] == nil {
-				continue
-			}
-			//var controls []Control
-			pm.logger.Info("Executing policy", "controls", annotation.Custom["controls"])
-			v, ok := annotation.Custom["controls"].([]interface{})
-			if !ok {
-				panic("controls is not an array")
-			}
-			for _, c := range v {
-				w, ok := c.(map[string]interface{})
-				if !ok {
-					panic("control element is not a map")
-				}
-
-				pm.logger.Info("Executing policy", "control", w["description"])
-				control := Control{
-					Title:       w["title"].(string),
-					Description: w["description"].(string),
-				}
-
-				controls = append(controls, control)
-			}
-		}
-
 		result := Result{
 			Policy: Policy{
 				File:        module.Package.Location.File,
@@ -80,7 +54,8 @@ func (pm *PolicyManager) Execute(ctx context.Context, pluginNamespace string, in
 			},
 			AdditionalVariables: map[string]interface{}{},
 			Violations:          nil,
-			Controls:            controls,
+			Activities:          nil,
+			Risks:               nil,
 		}
 
 		regoArgs := []func(r *rego.Rego){
@@ -102,15 +77,57 @@ func (pm *PolicyManager) Execute(ctx context.Context, pluginNamespace string, in
 				moduleOutputs := expression.Value.(map[string]interface{})
 
 				for key, value := range moduleOutputs {
-					if !slices.Contains([]string{"violation"}, key) {
+					if !slices.Contains([]string{"violation", "activities", "risks"}, key) {
 						result.AdditionalVariables[key] = value
 					}
 				}
 
 				for _, tester := range moduleOutputs["violation"].([]interface{}) {
-					result.Violations = append(result.Violations, tester.(map[string]interface{}))
+					var violation Violation
+
+					contents, err := json.Marshal(tester.(map[string]interface{}))
+					if err != nil {
+						return nil, err
+					}
+
+					err = json.Unmarshal(contents, &violation)
+					if err != nil {
+						return nil, err
+					}
+
+					result.Violations = append(result.Violations, violation)
 				}
 
+				for _, tester := range moduleOutputs["activities"].([]interface{}) {
+					var activity Activity
+
+					contents, err := json.Marshal(tester.(map[string]interface{}))
+					if err != nil {
+						return nil, err
+					}
+
+					err = json.Unmarshal(contents, &activity)
+					if err != nil {
+						return nil, err
+					}
+
+					result.Activities = append(result.Activities, activity)
+				}
+
+				for _, tester := range moduleOutputs["risks"].([]interface{}) {
+					var risk Risk
+					contents, err := json.Marshal(tester.(map[string]interface{}))
+					if err != nil {
+						return nil, err
+					}
+
+					err = json.Unmarshal(contents, &risk)
+					if err != nil {
+						return nil, err
+					}
+
+					result.Risks = append(result.Risks, risk)
+				}
 			}
 		}
 		output = append(output, result)
