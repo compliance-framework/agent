c9a6a9b203b41194569fd558ffadb31eccb532d1
Harry Lincoln <harry.lincoln@container-solutions.com>
2025-01-14 15:22:59 +0000

    Expires for stream results

--- cmd/agent.go
+++ cmd/agent.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/compliance-framework/agent/internal/event"
-	"github.com/google/uuid"
 	"log"
 	"os"
 	"os/exec"
@@ -13,6 +11,9 @@ import (
 	"sync"
 	"syscall"
 	"time"
+
+	"github.com/compliance-framework/agent/internal/event"
+	"github.com/google/uuid"
 
 	"github.com/compliance-framework/agent/internal"
 	"github.com/compliance-framework/agent/runner"
@@ -422,6 +423,9 @@ func (ar *AgentRunner) runInstance() error {
 				Risks:        &res.Risks,
 				Logs:         &res.Logs,
 				Labels:       resultLabels,
+				// TODO Use scheduler time here, rather than loop time. This is intentionally left at 1 minute for now. Once cron scheduling is in, we should change it.
+				// This TODO mainly serves as a signal to increase chances of not missing the update later
+				Expires: time.Now().Add(1 * time.Minute).Add(24 * time.Hour),
 			}
 
 			// Publish findings to nats
